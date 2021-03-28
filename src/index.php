9bb571ff4ee3c9d7ad133718e8cf33f9eebfe8b3
aolinyang <allenyang2813@gmail.com>
2021-03-28 17:24:30 -0500

    initial client-server test

--- src/index.php
+++ src/index.php
@@ -5,22 +5,46 @@
 <?php
 
 // Create connection
-$conn = new mysqli("localhost", "Rigel", "Belt6502", "corpscrdemo");
+$conn = new mysqli("localhost", "root", "asdf1234", "corpscrdemo");
 // Check connection
 if ($conn->connect_error)
 {
 	die("Connection failed: " . $conn->connect_error);
 } 
 
-// Exectue SQL
-$sql = "SELECT * FROM users;";
-$result = $conn->query($sql);
+if (!isset($_POST['submit'])) {
+	die("get nay-nayed");
+}
 
-print($result);
+$_POST['uin'], $_POST['fname'], $_POST['lname'], $_POST['outfit'], $_POST['gradyear'])
+$uin_key = $_POST['uin'] . "_0";
+$conn->query("INSERT INTO users(uin_key,uin,firstname,lastname,outfit,gradyear) VALUES(" . $uin_key . "," . $_POST['uin'] . "," . $_POST['fname'] . "," . $_POST['lname'] . "," . $_POST['outfit'] . "," . $_POST['gradyear'] . ")");
+
+$result = $conn->query("SELECT * FROM users");
+while ( $row = $result->fetch_assoc() ) {
+    print_r($row);
+}
+
 ?>
 
-<b>PHP WRITE TO SERVER</b>
+<body>
 
+<b>PHP WRITE TO SERVER</b>
 <b>PHP GET UPDATED DATABASE</b>
 
+<form onsubmit="submitForm()" method="POST" action="index.php">
+	<label for="uin">UIN: </label>
+	<input type="number" id="uin" name="uin">
+	<label for="fname">First Name: </label>
+	<input type="text" id="fname" name="fname">
+	<label for="lname">Last Name: </label>
+	<input type="text" id="lname" name="lname">
+	<label for="outfit">Outfit: </label>
+	<input type="text" id="outfit" name="outfit">
+	<label for="gradyear">Grad Year: </label>
+	<input type="number" id="gradyear" name="gradyear">
+	<input type="submit" id="submit" name="submit">Submit</input>
+</form>
+
+</body>
 </html>
