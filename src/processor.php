d191e5d7440b823e500bf66024c5fadb00e58e12
aolinyang <allenyang2813@gmail.com>
2021-03-28 18:36:19 -0500

    hmm2

--- src/processor.php
+++ src/processor.php
@@ -15,15 +15,17 @@ if (!isset($_POST['submit'])) {
 echo "Hello World";
 
 $uin_key = $_POST['uin'] . "_0";
-$conn->query("INSERT INTO users(uin_key,uin,firstname,lastname,outfit,gradyear) VALUES(" . $uin_key . "," . $_POST['uin'] . "," . $_POST['fname'] . "," . $_POST['lname'] . "," . $_POST['outfit'] . "," . $_POST['gradyear'] . ")");
 
-echo $conn;
 echo $uin_key;
 echo $_POST['uin'];
 echo $_POST['fname'];
 echo $_POST['lname'];
 echo $_POST['outfit'];
 echo $_POST['gradyear'];
+
+$conn->query("INSERT INTO users(uin_key,uin,firstname,lastname,outfit,gradyear) VALUES(" . $uin_key . "," . $_POST['uin'] . "," . $_POST['fname'] . "," . $_POST['lname'] . "," . $_POST['outfit'] . "," . $_POST['gradyear'] . ")");
+
+echo $conn;
 
 $result = $conn->query("SELECT * FROM users");
 while ( $row = $result->fetch_assoc() ) {
