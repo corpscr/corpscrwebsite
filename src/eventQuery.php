c6db35199ea2cb7131831f0634cfa9e724164beb
aolinyang <allenyang2813@gmail.com>
2021-04-13 07:02:49 -0500

    added more to eventQuery

--- src/eventQuery.php
+++ src/eventQuery.php
@@ -1,42 +1,62 @@
 <html>
 <head>
     <title>CR Event Search</title>
+    <?php
+
+    include "connectSQL.php";
+
+    function getEventsTable() {
+        $resultArr = [];
+        if (strlen($_POST['searchcolumn']) != 0) {
+            $conn = connectToSQL();
+
+            // TODO: sanitize input
+            $searchColumn = $_POST['searchcolumn'];
+            $searchTerm = "'%" . $_POST['searchterm'] . "%'";
+            if (strcmp($searchColumn, "any") == 0) {
+                $searchColumn = "CONCAT(eventname, ' ', eventdesc, ' ', industry, ' ', careertype, ' ', cr_competency)";  // TODO: replace with actual names of columns
+            }
+
+            $queryString = 'SELECT * FROM Events WHERE ' . $searchColumn . ' LIKE ' . $searchTerm;
+            $result = $conn->query($queryString);
+            $conn->close();
+
+            while ($row = $result->fetch_assoc()) {
+                $resultArr[] = $row;
+            }
+
+            $conn->close();
+        }
+        return json_encode($resultArr);
+    }
+
+    ?>
+    <script type="text/javascript">
+        var events = <?php echo getEventsTable(); ?>
+    </script>
 </head>
 <body>
 <form onsubmit="submitForm()" method="POST" action="eventQuery.php">
-    <label for="searchcolumn">Column to search: </label>
     <select name="searchcolumn" id="searchcolumn">
-        
+        <option value="any">Any</option>
+        <option value="eventname">Event Name</option>
+        <option value="eventdesc">Event Description</option>
+        <option value="industry">Industry</option>
+        <option value="careertype">Career Type</option>
+        <option value="cr_competency">CR Competency</option>
     </select>
+    <input type="text" name="searchterm" id="searchterm">
+    <input type="submit" value="Search">
 </form>
-<?php
 
-include "connectSQL.php";
+<select name="entriesperpage" id="entriesperpage">
+    <option value="10">10</option>
+    <option value="20">20</option>
+    <option value="50">50</option>
+    <option value="100">100</option>
+</select>
 
-if (strlen($_POST['searchcolumn']) != 0) {
-    $conn = connectToSQL();
-
-    // TODO: sanitize input
-    $searchColumn = $_POST['searchcolumn'];
-    $searchTerm = "'%" . $_POST['searchterm'] . "%'";
-    if (strcmp($searchColumn, "any") == 0) {
-        $searchColumn = "CONCAT(eventname, ' ', industry, ' ', careertype, ' ', crcompetency)";  // TODO: replace with actual names of columns
-    }
-
-    $queryString = 'SELECT * FROM Events WHERE ' . $searchColumn . ' LIKE ' . $searchTerm;
-    $result = $conn->query($queryString);
-    $conn->close();
-
-    $resultArr = array();
-    while ($row = $result->fetch_assoc()) {
-        $resultArr[] = $row;
-    }
-
-    $resultArr = json_encode($resultArr);
-
-    $conn->close();
-}
-
-?>
+<table>
+</table>
 </body>
 </html>
