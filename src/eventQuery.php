3211fca2462981e8fd466c726a4c6bcfe6ca2f88
aolinyang <allenyang2813@gmail.com>
2021-04-13 06:36:53 -0500

    made some small changes

--- src/eventQuery.php
+++ src/eventQuery.php
@@ -1,25 +1,42 @@
+<html>
+<head>
+    <title>CR Event Search</title>
+</head>
+<body>
+<form onsubmit="submitForm()" method="POST" action="eventQuery.php">
+    <label for="searchcolumn">Column to search: </label>
+    <select name="searchcolumn" id="searchcolumn">
+        
+    </select>
+</form>
 <?php
 
 include "connectSQL.php";
 
-$conn = connectToSQL();
+if (strlen($_POST['searchcolumn']) != 0) {
+    $conn = connectToSQL();
 
-// sanitize input
-$searchColumn = $_POST['searchcolumn'];
-$searchTerm = "'%" . $_POST['searchterm'] . "%'";
-if (strcmp($searchColumn, "any") == 0) {
-    $searchColumn = "CONCAT(eventname, ' ', industry, ' ', careertype, ' ', crcompetency)";  // TODO: replace with actual names of columns
+    // TODO: sanitize input
+    $searchColumn = $_POST['searchcolumn'];
+    $searchTerm = "'%" . $_POST['searchterm'] . "%'";
+    if (strcmp($searchColumn, "any") == 0) {
+        $searchColumn = "CONCAT(eventname, ' ', industry, ' ', careertype, ' ', crcompetency)";  // TODO: replace with actual names of columns
+    }
+
+    $queryString = 'SELECT * FROM Events WHERE ' . $searchColumn . ' LIKE ' . $searchTerm;
+    $result = $conn->query($queryString);
+    $conn->close();
+
+    $resultArr = array();
+    while ($row = $result->fetch_assoc()) {
+        $resultArr[] = $row;
+    }
+
+    $resultArr = json_encode($resultArr);
+
+    $conn->close();
 }
-
-$queryString = 'SELECT * FROM Events WHERE ' . $searchColumn . ' LIKE ' . $searchTerm;
-$result = $conn->query($queryString);
-$conn->close();
-
-$resultArr = array();
-while ($row = $result->fetch_assoc()) {
-    $resultArr[] = $row;
-}
-
-$resultArr = json_encode($resultArr);
 
 ?>
+</body>
+</html>
